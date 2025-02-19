d97cd3a22f8885273ee4d60d404abb773eb60275
Chris Woodgate <christopher.woodgate@gmail.com>
2025-02-19 10:48:53 +0000

    Addition of call to comms_finalise() at 'stop' statements during input reads.
    (Makes MPI happy if we stop because of a problem with an input file.)

--- src/io.f90
+++ src/io.f90
@@ -3,7 +3,7 @@
 !                                                                      !
 ! Module containing input/output routines.                             !
 !                                                                      !
-! C. D. Woodgate,  Warwick                                        2024 !
+! C. D. Woodgate,  Warwick                                        2025 !
 !----------------------------------------------------------------------!
 module io
 
@@ -12,6 +12,7 @@ module io
   use command_line
   use display
   use netcdf
+  use comms
   
   implicit none
 
@@ -20,7 +21,7 @@ module io
   !--------------------------------------------------------------------!
   ! Subroutine to write software version, date, and time               !
   !                                                                    !
-  ! C. D. Woodgate,  Warwick                                      2023 !
+  ! C. D. Woodgate,  Warwick                                      2025 !
   !--------------------------------------------------------------------!
   subroutine write_info(point)
 
@@ -109,6 +110,7 @@ module io
     inquire(file=trim(filename), exist=exists)
 
     if (.not. exists) then
+      call comms_finalise()
       stop 'Could not find input file ' // trim(filename)
     end if
 
@@ -234,6 +236,7 @@ module io
     close(15)
 
     if (.not. any(check)) then
+      call comms_finalise()
       stop 'Missing parameter in input file'
     end if
 
@@ -384,6 +387,7 @@ module io
     open(25, file=filename, iostat=ios)
 
     if (ios .ne. 0) then
+      call comms_finalise()
       stop 'Could not parse input file. Aborting...'
     end if
 
@@ -456,6 +460,7 @@ module io
     open(25, file=filename, iostat=ios)
 
     if (ios .ne. 0) then
+      call comms_finalise()
       stop 'Could not parse tmmc input file. Aborting...'
     end if
 
@@ -537,6 +542,7 @@ module io
     close(25)
 
     if (.not. any(check)) then
+      call comms_finalise()
       stop 'Missing parameter in tmmc input file'
     end if
 
@@ -562,6 +568,7 @@ module io
     open(25, file=filename, iostat=ios)
 
     if (ios .ne. 0) then
+      call comms_finalise()
       stop 'Could not parse wang landau input file. Aborting...'
     end if
 
@@ -662,6 +669,7 @@ module io
     close(25)
 
     if (.not. any(check)) then
+      call comms_finalise()
       stop 'Missing parameter in wang landau input file'
     end if
 
@@ -687,6 +695,7 @@ module io
     open(25, file=filename, iostat=ios)
 
     if (ios .ne. 0) then
+      call comms_finalise()
       stop 'Could not parse energy spectrum input file. Aborting...'
     end if
 
@@ -732,6 +741,7 @@ module io
     close(25)
 
     if (.not. any(check)) then
+      call comms_finalise()
       stop 'Missing parameter in energy spectrum input file'
     end if
 
