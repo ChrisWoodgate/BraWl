419a0ba17d05e59ec001eb97a859c1975c466c9b
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2025-04-15 16:33:15 +0100

    Added exit statement since module is WIP and does not function

--- src/tmmc.f90
+++ src/tmmc.f90
@@ -18,7 +18,9 @@ module tmmc
 
   implicit none
 
-contains
+  contains
+
+  
 
   !------------------------------------------------------------------!
   ! Main TMMC routine.                                               !
@@ -57,6 +59,14 @@ contains
     ! window variables
     integer, allocatable :: window_indices(:, :)
     integer :: num_windows, num_walkers
+
+    if (my_rank == 0) then
+      write (6, '(72("~"))')
+      write (6, '(5("~"),x,"Error: WIP module. Does not function.",x,6("~"))')
+      write (6, '(72("~"))')
+    end if
+    call MPI_FINALIZE(ierror)
+    call EXIT(0)
 
     ! Get number of MPI processes
     call MPI_COMM_SIZE(MPI_COMM_WORLD, num_proc, ierror)
