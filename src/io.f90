ef1a088b1298cceec9f2a59d93824eaa2a63bc86
Chris Woodgate <christopher.woodgate@gmail.com>
2025-04-15 15:50:21 +0100

    Removing unnecessary call to parse_args() in src/io.f90
    This is a private function and should not be called outside of the
    command_line module.

--- src/io.f90
+++ src/io.f90
@@ -457,9 +457,6 @@ module io
     integer :: my_rank
     character(len=30) :: control = ' '
 
-    ! Parse the command line arguments
-    call parse_args()
-
     ! Parse the name of the input file
     if(my_rank == 0) then
       write(6,'(/,17("-"),x,"Parsing name of Metropolis input file",x,16("-"),/)')
