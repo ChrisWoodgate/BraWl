a096290504011a386e8cf281ff52d51444ce536e
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2025-04-16 14:55:23 +0100

    Removed calls to deleted functionality

--- src/main.f90
+++ src/main.f90
@@ -14,8 +14,6 @@ program main
   use nested_sampling
   use tmmc
   use wang_landau
-  use energy_spectrum
-  use config_output
   use io
   use kinds
   use c_functions
@@ -40,9 +38,6 @@ program main
 
   ! Wang Landau parameters type
   type(wl_params) :: wl_setup
-
-  ! Energy Spectrum parameters type
-  type(es_params) :: es_setup
 
   ! Start MPI
   call comms_initialise()
@@ -109,17 +104,6 @@ program main
     ! Wang Landau algorithm
     call read_wl_file("wl_input.txt", wl_setup, my_rank)
     call wl_main(setup, wl_setup)
-
-  else if (setup%mode == 306) then
-
-    ! Energy spectrum algorithm
-    call read_es_file("es_input.txt", es_setup, my_rank)
-    call es_main(setup, es_setup, my_rank)
-
-  else if (setup%mode == 307) then
-
-    ! Configuration at temperature output
-    call co_main(setup, my_rank)
 
   else
 
